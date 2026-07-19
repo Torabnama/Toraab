186aff3ab324a599d1b0b9f0f654f976b52816a1
Torabnama <babadeakhe@gmail.com>
2026-07-19 15:59:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -100,3 +100,68 @@ const gallerySwiper = new Swiper(".gallerySwiper", {
 
     speed: 700,
 });
+        document.querySelector("#ticket").scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+}
+
+/*==============================
+      انیمیشن ورود پوستر
+==============================*/
+
+const poster = document.querySelector(".show-header img");
+
+if(poster){
+
+    poster.animate(
+
+        [
+
+            {
+                transform:"scale(1.08)",
+                opacity:.7
+            },
+
+            {
+                transform:"scale(1)",
+                opacity:1
+            }
+
+        ],
+
+        {
+
+            duration:900,
+
+            easing:"ease-out",
+
+            fill:"forwards"
+
+        }
+
+    );
+
+}
+
+const gallerySwiper = new Swiper(".gallerySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".gallery-pagination",
+        clickable: true,
+    },
+
+    speed: 700,
+});
